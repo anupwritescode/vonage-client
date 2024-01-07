89962356409079ba51bccd709d687aba71568b62
Anup Salve <anupsalve8@gmail.com>
2024-01-07 23:02:39 +0530

    hide jwt from code

--- src/CreateConv.js
+++ src/CreateConv.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import jwt from './jwt';
 
 function CreateConv() {
     const [convName, setConvName] = useState('');
@@ -17,7 +18,7 @@ function CreateConv() {
                 },
                 {
                     headers: {
-                        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDQ2NDQ3NzgsImV4cCI6MTcwNTE2MzE3OCwianRpIjoiRWR5ZUdlN2ZpdlJyIiwiYXBwbGljYXRpb25faWQiOiIzYTcyMmZjYy0yMzVjLTQ3MmQtOTExMS0wOTZmYzc4N2M5NmIiLCJzdWIiOiIiLCJhY2wiOnsicGF0aHMiOnsiLyovc2Vzc2lvbnMvKioiOnt9LCIvKi91c2Vycy8qKiI6e30sIi8qL2NvbnZlcnNhdGlvbnMvKioiOnt9fX19.ZXIIG0r2V9D-aajlk-PCIiNTNAXPTkYp4PqJb7fPZOsMqrMt9tMrFUfJrAeza7V1IqMn-VOXDDbYEgov1xXz38b0qUhPpjslzaRgQ9CIEyemO_ZmkLWyjcnTs_InO1nOOLv_on9VlSxx1NhB7i2sqX2Qczc0OUBhDFURjqeOjU4xxRbgI2wT4DNZu2j0ddBwXUfVT6yiIEJ4xu64UMMPfg5VaGlNHYmtM4xVMmZr2Q5tDpxSwd53E-fhiDRsaR8aYMsmnMnm-WhIRixjPRM2tbUM1IitZGfWkqQIdf_9ywawciFYatQmTFgasHuRM5l-m2BfAsnZNPEwHeaWDFyLQg'
+                        'Authorization': 'Bearer ' + jwt
                     }
                 });
 
--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './CreateUser.css';
+import jwt from './jwt';
 
 function CreateUser() {
     const [userName, setUserName] = useState('');
@@ -18,7 +19,7 @@ function CreateUser() {
                 },
                 {
                     headers: {
-                        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDQ2NDQ3NzgsImV4cCI6MTcwNTE2MzE3OCwianRpIjoiRWR5ZUdlN2ZpdlJyIiwiYXBwbGljYXRpb25faWQiOiIzYTcyMmZjYy0yMzVjLTQ3MmQtOTExMS0wOTZmYzc4N2M5NmIiLCJzdWIiOiIiLCJhY2wiOnsicGF0aHMiOnsiLyovc2Vzc2lvbnMvKioiOnt9LCIvKi91c2Vycy8qKiI6e30sIi8qL2NvbnZlcnNhdGlvbnMvKioiOnt9fX19.ZXIIG0r2V9D-aajlk-PCIiNTNAXPTkYp4PqJb7fPZOsMqrMt9tMrFUfJrAeza7V1IqMn-VOXDDbYEgov1xXz38b0qUhPpjslzaRgQ9CIEyemO_ZmkLWyjcnTs_InO1nOOLv_on9VlSxx1NhB7i2sqX2Qczc0OUBhDFURjqeOjU4xxRbgI2wT4DNZu2j0ddBwXUfVT6yiIEJ4xu64UMMPfg5VaGlNHYmtM4xVMmZr2Q5tDpxSwd53E-fhiDRsaR8aYMsmnMnm-WhIRixjPRM2tbUM1IitZGfWkqQIdf_9ywawciFYatQmTFgasHuRM5l-m2BfAsnZNPEwHeaWDFyLQg'
+                        'Authorization': 'Bearer ' + jwt
                     }
                 });
 
